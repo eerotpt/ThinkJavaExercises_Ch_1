a948285b8c2c2015b5c4f23188e9525de780b33b
eerotpt <eero.sakson@tptlive.ee>
2016-11-13 19:35:42 +0200

    13/11/2016

---
+++ src/Exercises_Ch_1.java
@@ -0,0 +1,21 @@
+/**
+ * Created by a on 09/11/2016.
+ */
+public class Exercises_Ch_1 {
+    /**Exercise 1.1
+     *            1.In computer jargon a statement is code that performs operation,
+     *               a comment is description of a part of code that has no effect
+     *               to the workings of the program, while statement does exactly that.
+     *            2.A program that is portable can be run on different machines.
+     *            3.Compile is changing the high-language program to low-language,
+     *            changing program from words to numbers so to say.
+     *            4.Executable means that program is made to run on specific hardware
+     *
+      */
+
+    //Exercise 1.2
+    public static void main(String[] args){
+        System.out.print("Hello , World! "); // a comment
+        System.out.print("How are you?");//another comment
+    }
+}
